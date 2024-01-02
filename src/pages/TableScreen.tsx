e8dc8ce3d7a570d200c3e086d0b83dfdc579e22c
Manuel Morandin <manuel.morandin@gmail.com>
2024-01-02 23:21:08 +1100

    clean code

--- src/pages/TableScreen.tsx
+++ src/pages/TableScreen.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { UsersTable } from '../component/Table/UsersTable';
-import { UserData } from '../types';
-import { useLocation } from 'react-router-dom';
 import { Box } from '@mui/material';
 import useSocket from '../hooks/useSocketConnection';
 import { useAppData } from '../hooks/useStores';
@@ -9,12 +7,10 @@ import { useAppData } from '../hooks/useStores';
 interface TableScreenType {}
 
 export const TableScreen: React.FC<TableScreenType> = ({}) => {
-  const location = useLocation();
   const { userData, lastUpdatedUserID, sliderValue, deleteUserData } =
     useAppData();
 
   useSocket('ws://localhost:3001');
-  console.log('pathname: ', location.pathname);
 
   return (
     <Box sx={{ p: 3 }}>
